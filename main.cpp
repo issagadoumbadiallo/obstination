9e9d9212278304c790bdece237dc174705ce4d6c
issagadoumbadiallo <issagadoumbadiallo@gmail.com>
2016-08-25 14:52:19 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+//#include <iostream>
+//#include "include/GlobalConst.h"
+#include <vector>
+
+void tt(){
+    l<<"Trying tt voidFunc";
+}
+
+int main() {
+    l << "Starting Main";
+
+    std::vector<int> v ;
+
+
+    tt();
+    return 0;
+}
+
+
+/*
+int main() {
+    LivingObj l= LivingObj();
+    l << "hello" ;
+
+    l << "1"; //"Test";
+
+    return 0;
+}*/
