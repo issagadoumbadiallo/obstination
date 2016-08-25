c9afbcc3f887673f9667e044321a801a19de836a
issagadoumbadiallo <issagadoumbadiallo@gmail.com>
2016-08-25 14:58:00 +0200

    /include

---
+++ GlobalConst.h
@@ -0,0 +1,11 @@
+//
+// Created by idiallo on 03/08/2016.
+//
+
+#ifndef HELLOCLION_GLOBALCONST_H
+#define HELLOCLION_GLOBALCONST_H
+
+#endif //HELLOCLION_GLOBALCONST_H
+#include "spdlog/log.h"
+
+LivingObj l;
