49b0b057be20064f70548ebfb49bdfe8ac475e0f
issagadoumbadiallo <issagadoumbadiallo@gmail.com>
2016-08-26 15:53:34 +0200

    Add files via upload

--- include/spdlog/log.h
+++ include/spdlog/log.h
@@ -2,6 +2,14 @@
 //#include "logger.h"
 //#include "tweakme.h"
 //#include "common.h"
+#include <chrono>
+#include <ctime>
+
+
+#include <iostream>
+#include <iomanip>
+//#include <thread>
+
 
 class LivingObj {
     std::shared_ptr<spdlog::logger> file;
@@ -24,5 +32,50 @@ public:
         file->info(msg);
     };*/
 
+};
+
+//using namespace std;
+
+class Duree{
+
+private:
+    std::clock_t c_start, c_end;
+    std::chrono::_V2::system_clock::time_point t_start, t_end; //=std::clock();
+    double c_duration=0;
+public:
+            duree(){
+                c_start= std::clock();
+                t_start=std::chrono::high_resolution_clock::now();
+                //initialisation de la durée
+                c_duration=0;
+                t_duration=0;
+            }
+
+            void restart(){
+                this->duree();
+            }
+            void end(){
+                c_end = std::clock();
+                t_end = std::chrono::high_resolution_clock::now();
+                c_duration= 1000.0 * (c_end-c_start) / CLOCKS_PER_SEC;
+                t_duration=std::chrono::duration<double, std::milli>(t_end-t_start).count();
+            }
+
+            double duree_Clock(){
+                return c_duration;
+            }
+
+                double duree_time(){
+                return t_duration;
+            }
+
+            //void start(){ this.Duree();}
+            show_all(){
+                std::cout << std::fixed << std::setprecision(2) << "CPU time used: "
+                          << c_duration << " ms\n"
+                          << "Wall clock time passed: "
+                          << t_duration
+                          << " ms\n";
+            }
 };
 
